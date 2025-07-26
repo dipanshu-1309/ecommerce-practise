50238d4e8847bc9638177c42eefc31025de4abc1
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-07-26 18:28:52 +0530

    usedata from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import Header from "../components/Header";
 import "./HomePage.css";
-import { products } from "../../starting-code/data/products";
+import axios from "axios";
+import { useEffect,useState } from "react";
+
 
 const HomePage = () => {
 
-  fetch('http://localhost:3000/api/products').then((response)=>{
-    return response.json();
-  }).then((data)=>{
-      console.log(data);
-  });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+      axios.get('http://localhost:3000/api/products').then((response)=>{
+      setProducts(response.data);
+     });
+  },[]);
 
   return (
     <>
