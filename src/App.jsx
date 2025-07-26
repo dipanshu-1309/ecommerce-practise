3201ddbeb9e3c37f1f02d549d67687ad843c175d
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-07-26 15:53:58 +0530

    orders and tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { Route, Routes } from 'react-router'
 import HomePage from "./pages/HomePage"
 import './index.css'
 import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
+
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </div>
   )
